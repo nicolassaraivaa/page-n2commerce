5e2fba7360353932fc6ab221d01666d9031f3209
Nicolas Moro Ruiz <nicolasmororuiz@gmail.com>
2025-10-26 01:44:11 -0300

    feat: integrate ScrollProgress component into Home layout for enhanced user experience

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,10 +6,12 @@ import Hero from "./components/common/Hero";
 import HowItWorks from "./components/common/HowltWorks";
 import Pricing from "./components/common/Pricings";
 import Testimonials from "./components/common/Testimonials";
+import { ScrollProgress } from "./components/ui/scroll-progress";
 
 export default function Home() {
   return (
-    <div id="inicio" className="overflow-x-hidden">
+    <div id="inicio" className="overflow-x-hidden relative">
+      <ScrollProgress />
       <Header />
       <Hero />
       <HowItWorks />
